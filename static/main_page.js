6af2bf4a24a7c4b91a557ee9b8be5272ba75a1ac
Martin 'A-Bone' Ahrnbom <m.ahrnbom@outlook.com>
2021-02-06 12:58:49 +0100

    now possible to host games!

--- static/main_page.js
+++ static/main_page.js
@@ -1,3 +1,18 @@
+function httpGetAsync(url, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4) {
+            if (xmlHttp.status == 200)
+                callback(xmlHttp.responseText);
+            else 
+                alert("Server error on url " + url + " with " + xmlHttp.status + " and message '" + xmlHttp.responseText);
+        } 
+    }
+    xmlHttp.open("GET", url, true); 
+    xmlHttp.send(null);
+}
+
 function n_players_changed() {
     var host_input = document.getElementById("host_n_players");
     var num = parseInt(host_input.value);
@@ -24,9 +39,24 @@ function host_game() {
     var host_names = document.getElementById("host_names");
     var names = [];
     var inputs = host_names.getElementsByTagName("*");
+    var ok = true;
     for (var i = 0; i < inputs.length; ++i) {
-        names.push(inputs[i].value);
+        var name = inputs[i].value;
+        if (name.indexOf(',') > -1) {
+            ok = false;
+        } else
+            names.push(name);
     }
 
-    alert(names);
+    if (ok) {
+        httpGetAsync("host?players="+String(names), host_response);
+    } else {
+        alert("Some illegal characters in name!")
+    }
+    
+}
+
+function host_response() {
+    var p = document.getElementById("host_response");
+    p.innerHTML = "Hosted game successfully!";
 }
